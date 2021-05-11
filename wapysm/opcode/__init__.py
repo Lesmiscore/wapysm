6b2d812a1624a41a0c64f36118dd0c8caadd9508
nao20010128nao <nao20010128@gmail.com>
2021-05-11 12:02:28 +0900

    define 2.4.5 Control Instructions

--- wapysm/opcode/__init__.py
+++ wapysm/opcode/__init__.py
@@ -1,19 +1,61 @@
 # 2.4 Instructions
-# This defines structure of all instructions and action itself,
+# This defines structure of all instructions,
 # and not responsible for parsing binaries or texts.
 
+from typing import List
+from wapysm.parser.structure import VALTYPE_TYPE
+
+
 class InstructionBase(object):
-    def execute(self):
-        pass
+    pass
 
 # 2.4.5 Control Instructions
 
 class Unreachable(InstructionBase):
     "unreachable"
-    def execute(self):
-        raise Exception('Unreachable!')
 
 class Nop(InstructionBase):
     "nop"
-    def execute(self):
-        pass
+
+class BlockInstructionBase(InstructionBase):
+    resultype: List[VALTYPE_TYPE] = []
+    instr: List[InstructionBase] = []
+
+class Block(BlockInstructionBase):
+    "block"
+
+class Loop(BlockInstructionBase):
+    "loop"
+
+class IfElse(BlockInstructionBase):
+    "if_else"
+    else_block: List[InstructionBase] = []
+
+
+class BranchInstructionBase(InstructionBase):
+    labelidx: int = 0
+
+class Br(BlockInstructionBase):
+    "br"
+
+class BrIf(BlockInstructionBase):
+    "br_if"
+
+class BrTable(BlockInstructionBase):
+    "br_table"
+    labelindices: List[int] = []
+
+class Return(BlockInstructionBase):
+    "return"
+
+
+class CallInstructionBase(BranchInstructionBase):
+    pass
+
+class Call(CallInstructionBase):
+    "call"
+    callidx: int = 0
+
+class CallIndirect(CallInstructionBase):
+    "call_indirect"
+    typeidx: int = 0
