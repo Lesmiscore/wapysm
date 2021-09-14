b08f36fb68b01ae2217d6ac0f4748e2c5e2b1ca9
nao20010128nao <nao20010128@gmail.com>
2021-09-15 00:07:56 +0900

    fix wrong opcode table

--- tests/test_binary_instructions.py
+++ tests/test_binary_instructions.py
@@ -7,5 +7,10 @@ from wapysm.parser.binary.instruction import OPCODE_TABLE
 
 class TestBinaryInstructions(unittest.TestCase):
     def test_opcode_table(self):
+        values = {}
+        for x in OPCODE_TABLE.values():
+            values.setdefault(x, 0)
+            values[x] += 1
+        print({k: v for k, v in values.items() if v > 1})
         self.assertEqual(len(OPCODE_TABLE), len(set(OPCODE_TABLE.keys())))
         self.assertEqual(len(OPCODE_TABLE), len(set(OPCODE_TABLE.values())))
