8eb00a9bc1a396ff1a6509c378234298a512737e
nao20010128nao <nao20010128@gmail.com>
2021-09-16 00:08:39 +0900

    sort functions

--- tests/test_run_wasm_file.py
+++ tests/test_run_wasm_file.py
@@ -12,17 +12,16 @@ from wapysm.execute.context import WasmGlobalInstance, WasmTable
 from wapysm.execute.interpreter.invocation import wrap_function
 
 class TestRunWasmFile(unittest.TestCase):
-    def test_table(self):
-        wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/table.wasm').content
+
+    def test_fail(self):
+        wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/fail.wasm').content
         wasm = WebAssembly.instantiate(wasm_bin, {})
 
-        exp_table = cast(WasmTable, wasm.module.exports[0].value)
-        print(exp_table)
-        print(exp_table.elem)
-        elem_0 = wrap_function(exp_table.elem[0], wasm.module.store)
-        elem_1 = wrap_function(exp_table.elem[1], wasm.module.store)
-        self.assertEqual(cast(WASM_VALUE, elem_0())[2], 13)
-        self.assertEqual(cast(WASM_VALUE, elem_1())[2], 42)
+        try:
+            wasm.exports['fail_me']()
+            raise AssertionError('This invocation must fail.')
+        except Exception:
+            print('Success!')
 
     def test_global(self):
         wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/global.wasm').content
@@ -47,12 +46,14 @@ class TestRunWasmFile(unittest.TestCase):
         wasm.exports['incGlobal']()
         self.assertEqual(cast(WASM_VALUE, wasm.exports['getGlobal']())[2], 44)
 
-    def test_fail(self):
-        wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/fail.wasm').content
+    def test_table(self):
+        wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/table.wasm').content
         wasm = WebAssembly.instantiate(wasm_bin, {})
 
-        try:
-            wasm.exports['fail_me']()
-            raise AssertionError('This invocation must fail.')
-        except Exception:
-            print('Success!')
+        exp_table = cast(WasmTable, wasm.module.exports[0].value)
+        print(exp_table)
+        print(exp_table.elem)
+        elem_0 = wrap_function(exp_table.elem[0], wasm.module.store)
+        elem_1 = wrap_function(exp_table.elem[1], wasm.module.store)
+        self.assertEqual(cast(WASM_VALUE, elem_0())[2], 13)
+        self.assertEqual(cast(WASM_VALUE, elem_1())[2], 42)
