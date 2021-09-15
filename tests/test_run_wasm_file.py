5ce8ff6e98f747abefe94d45257e93787b868850
nao20010128nao <nao20010128@gmail.com>
2021-09-16 00:08:11 +0900

    add DIV/0 fail test

--- tests/test_run_wasm_file.py
+++ tests/test_run_wasm_file.py
@@ -19,8 +19,8 @@ class TestRunWasmFile(unittest.TestCase):
         exp_table = cast(WasmTable, wasm.module.exports[0].value)
         print(exp_table)
         print(exp_table.elem)
-        elem_0 = wrap_function(exp_table.elem[0], wasm.module.store, ['i32'])
-        elem_1 = wrap_function(exp_table.elem[1], wasm.module.store, ['i32'])
+        elem_0 = wrap_function(exp_table.elem[0], wasm.module.store)
+        elem_1 = wrap_function(exp_table.elem[1], wasm.module.store)
         self.assertEqual(cast(WASM_VALUE, elem_0())[2], 13)
         self.assertEqual(cast(WASM_VALUE, elem_1())[2], 42)
 
@@ -46,3 +46,13 @@ class TestRunWasmFile(unittest.TestCase):
 
         wasm.exports['incGlobal']()
         self.assertEqual(cast(WASM_VALUE, wasm.exports['getGlobal']())[2], 44)
+
+    def test_fail(self):
+        wasm_bin = requests.get('https://github.com/mdn/webassembly-examples/raw/master/js-api-examples/fail.wasm').content
+        wasm = WebAssembly.instantiate(wasm_bin, {})
+
+        try:
+            wasm.exports['fail_me']()
+            raise AssertionError('This invocation must fail.')
+        except Exception:
+            print('Success!')
